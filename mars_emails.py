febf5bade796397e5990ae7d67d00ea14dbd6b10
ARYAN JAIN <117700812+gitsofaryan@users.noreply.github.com>
2023-12-10 15:35:00 +0530

    Update mars_emails.py

--- mars_emails.py
+++ mars_emails.py
@@ -4,7 +4,7 @@ import requests
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def get_mars_photos(sol, count=3, api_key='nVhVZFMbWNNatVph2D1f62FEsxrXKWbvpT2yU0tz'):
+def get_mars_photos(sol, count=3, api_key='NASA_API_KEY'):
     rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
     params = {'sol': sol, 'api_key': api_key}
     response = requests.get(rover_url, params=params).json()
@@ -26,9 +26,9 @@ def send_mars_email(api_key, from_email, to_email, img_urls):
     print(response.status_code, response.body, response.headers)
 
 if __name__ == '__main__':
-    sendgrid_api_key = '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'
-    from_email = 'mail.aryan.jain07@gmail.com'
-    to_email = 'aryan.jain.csbs22@ggits.net'
+    sendgrid_api_key = 'YOUR_SENDGRID_API'
+    from_email = 'SENDER_MAIL'
+    to_email = 'RECEIVER_MAIL'
     sol = 1000
     photo_count = 10
 
